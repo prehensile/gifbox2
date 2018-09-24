514bc8f67be82bf780ca8d01b923ff397c4794cc
prehensile <me@prehensile.co.uk>
2018-09-24 15:01:46 +0100

    utility scripts for running server

---
+++ server-local/run-devserver.sh
@@ -0,0 +1,5 @@
+export FLASK_APP=main.py
+export FLASK_ENV=development
+export FLASK_RUN_PORT=5000
+export FLASK_RUN_HOST=0.0.0.0
+flask run
--- server-local/run-server.sh
+++ server-local/run-server.sh
@@ -1,5 +1,7 @@
 #source .venv/bin/activate
 #pip install -r requirements.txt
 export FLASK_APP=main.py
-export FLASK_ENV=development
+export FLASK_ENV=production
+export FLASK_RUN_PORT=80
+export FLASK_RUN_HOST=0.0.0.0
 flask run
