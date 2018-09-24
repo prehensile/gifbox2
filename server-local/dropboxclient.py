03f7061608f8e847be01f353cf12d2d7daf2e53a
prehensile <me@prehensile.co.uk>
2018-09-24 15:03:04 +0100

    added thread for update_forever

--- server-local/dropboxclient.py
+++ server-local/dropboxclient.py
@@ -1,6 +1,8 @@
 import os
 import datetime
 import logging
+import threading
+import time
 
 import dropbox
 
@@ -9,12 +11,20 @@ class DropboxClient( object ):
 
 
     def __init__( self, access_token=None, media_path_local=None, media_path_remote=None  ):
+        
         self._dbx = dropbox.Dropbox( access_token )
         self._media_path_remote = media_path_remote
         self._media_path_local = media_path_local
 
+        # threading stuff...
+        self._run_flag = None
+        self._thread = None
+
 
     def _local_path_for_metadata( self, file_metadata ):
+        """
+        Returns a local path (in media folder) for a remote Dropbox file
+        """
         return os.path.join(
             self._media_path_local,
             file_metadata.name
@@ -22,6 +32,10 @@ class DropboxClient( object ):
 
 
     def download_file( self, file_metadata ):
+        """
+        Download the Dropbox file specified by file_metadata
+        Local path is inferred using _local_path_for_metadata().
+        """
         # TODO: queue this
         local_path = self._local_path_for_metadata( file_metadata )
         print( "Will download {} to {}".format( file_metadata.path_lower, local_path ) )
@@ -32,29 +46,49 @@ class DropboxClient( object ):
 
 
     def sync_from_server( self ):
-        
+        """
+        Perform a single sync from Dropbox to local media folder.
+        Downloads all files that don't exist locally, or are newer than local equivalents.
+        """
+
+        # TODO: remove deleted files
+
+        # list all remote files
         resp = self._dbx.files_list_folder( self._media_path_remote )
         
+        # step through remote files
         for file_metadata in resp.entries:
             
+            # by default, download files
             should_download_file = True
             
+            # get possible local path for remote file
             local_path = self._local_path_for_metadata( file_metadata )
             
+            # if this path already exists locally...
             if os.path.exists( local_path ):
 
+                # get last modification date for local file
                 s = os.stat( local_path )
                 dt_local = datetime.datetime.fromtimestamp( s.st_mtime )
 
+                # if remote file is older, or the same age as local file...
                 if file_metadata.server_modified <= dt_local:
+                    # ..don't download it
                     should_download_file = False
 
             if should_download_file:
                 self.download_file( file_metadata )
+        
+        # return cursor from initial listing so we could e.g. use it for getting updates
+        return resp.cursor
 
 
     def files_since( self, dt ):
-
+        """
+        Returns a list of all locally downloaded files which have been modified
+        since the timestamp specified in dt (a datetime.datetime object).
+        """
         new_files = []
         
         # sort local media files by modify date, descending
@@ -76,14 +110,60 @@ class DropboxClient( object ):
         return new_files
     
 
-    # TODO: this should probably run on a thread?
-    def update( self, cursor ):
-        while True:
+    # this should probably run on a thread?
+    def update_forever( self, flag ):
+        logging.debug( "DropboxClient.update_forever()" )
+        # do initial update, get cursor
+        cursor = self.sync_from_server()
+        while not flag.is_set():
+            
+            logging.debug( "DropboxClient start long poll..." )
+            
             longpoll_res = self._dbx.files_list_folder_longpoll( cursor, timeout=30 )
+            
+            logging.debug( "... result is: %r", longpoll_res )
+            
+            # if longpoll reports any changes...
             if longpoll_res.changes:
+                
+                # TODO: deal with deletions
+
+                # list changed files
                 list_res = self._dbx.files_list_folder_continue( cursor )
+                
+                # download changed files
                 for file_metadata in list_res.entries:
                     self.download_file( file_metadata )
+                
+                # set cursor
+                cursor = list_res.cursor
+            
+            # backoff if asked to
+            if longpoll_res.backoff:
+                time.sleep( longpoll_res.backoff )
+            
+            logging.debug( "... DropboxClient long poll ended, start another" )
+        
+        logging.debug( "... DropboxClient.update_forever() finished" )
+    
+
+    def run( self ):
+        logging.debug( "DropboxClient.run()" )
+        self._run_flag = threading.Event()
+        self._thread = threading.Thread(
+            name='DropboxDaemon',
+            target=self.update_forever,
+            daemon=True,
+            args=[self._run_flag]
+        )
+        self._thread.start()
+    
+
+    def stop( self ):
+        logging.debug( "DropboxClient.stop()" )
+        if self._run_flag and self._thread:
+            self._run_flag.set()
+            self._thread.join()
 
 
 def main():
