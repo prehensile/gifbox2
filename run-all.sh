302e72a6017f38ba2b2cc926e815c45a6d4af94d
prehensile <me@prehensile.co.uk>
2018-09-25 20:52:20 +0100

    run server in a screen session (for debugging)

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,4 @@
 export GIFBOX_MEDIA=/home/pi/gifbox2/media
-cd ~/gifbox2/server-local
-./run-server.sh &
+screen -dm bash -c 'cd ~/gifbox2/server-local; ./run-server.sh; exec sh'
 ## will invoke xinitrc.sh 
 startx -- -nocursor
