35f5732443f90215ee29be46cbc63d3243124a7f
prehensile <me@prehensile.co.uk>
2018-12-03 23:01:45 +0000

    start og bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+# install WiFi Connect
+# - creates a wifi hotspot for configuration when device can't connect to wifi
+bash <(https://raw.githubusercontent.com/balena-io/wifi-connect/master/scripts/raspbian-install.sh)
